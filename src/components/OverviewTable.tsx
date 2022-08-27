0f09837ff1ebe02c1c88abb863c4c1b559be556f
ical10 <mochhusnir@gmail.com>
2022-08-27 12:27:52 +0700

    feat: created initial components

---
+++ src/components/OverviewTable.tsx
@@ -0,0 +1,256 @@
+import FirstPageIcon from '@mui/icons-material/FirstPage';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import LastPageIcon from '@mui/icons-material/LastPage';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableFooter from '@mui/material/TableFooter';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import Typography from '@mui/material/Typography';
+import {useTheme} from '@mui/material/styles';
+
+import {useState} from 'react';
+
+import PropTypes from 'prop-types';
+
+function TablePaginationActions(props) {
+  const theme = useTheme();
+  const {count, page, rowsPerPage, onPageChange} = props;
+
+  const handleFirstPageButtonClick = event => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = event => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = event => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = event => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <Box sx={{flexShrink: 0, ml: 2.5}}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
+        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </Box>
+  );
+}
+
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
+
+type TableDataProps = {
+  implementers: string;
+  date: Date;
+  ghReport: string;
+  domain: string;
+  urlScanLink: string;
+  isTakenDown: boolean;
+  isConfirmed: boolean;
+  isEligibility: boolean;
+};
+
+const OverviewTable = () => {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [rows] = useState<TableDataProps[]>([
+    {
+      implementers: 'Brooklyn Simmons',
+      date: new Date('2015-05-27'),
+      ghReport: 'PR 1411',
+      domain: 'skycloud.com',
+      urlScanLink: 'https://urlscan.io/result/011bbfcd-0ada-4fca-9daa-b7ed4f5da935/dom/',
+      isTakenDown: true,
+      isConfirmed: false,
+      isEligibility: true,
+    },
+    {
+      implementers: 'Diane Russell',
+      date: new Date('2022-07-01'),
+      ghReport: 'PR 1411',
+      domain: 'connectweb3.com',
+      urlScanLink: 'https://urlscan.io/result/011bbfcd-0ada-4fca-9daa-b7ed4f5da935/dom/',
+      isTakenDown: false,
+      isConfirmed: true,
+      isEligibility: false,
+    },
+    {
+      implementers: 'Jacob Jones',
+      date: new Date('2021-01-30'),
+      ghReport: 'PR 1411',
+      domain: 'supercpu.com',
+      urlScanLink: 'https://urlscan.io/result/011bbfcd-0ada-4fca-9daa-b7ed4f5da935/dom/',
+      isTakenDown: false,
+      isConfirmed: false,
+      isEligibility: false,
+    },
+    {
+      implementers: 'Brooklyn Simmons',
+      date: new Date('2015-05-27'),
+      ghReport: 'PR 1411',
+      domain: 'skycloud.com',
+      urlScanLink: 'https://urlscan.io/result/011bbfcd-0ada-4fca-9daa-b7ed4f5da935/dom/',
+      isTakenDown: true,
+      isConfirmed: false,
+      isEligibility: true,
+    },
+    {
+      implementers: 'Diane Russell',
+      date: new Date('2022-07-01'),
+      ghReport: 'PR 1411',
+      domain: 'connectweb3.com',
+      urlScanLink: 'https://urlscan.io/result/011bbfcd-0ada-4fca-9daa-b7ed4f5da935/dom/',
+      isTakenDown: false,
+      isConfirmed: true,
+      isEligibility: false,
+    },
+    {
+      implementers: 'Jacob Jones',
+      date: new Date('2021-01-30'),
+      ghReport: 'PR 1411',
+      domain: 'supercpu.com',
+      urlScanLink: 'https://urlscan.io/result/011bbfcd-0ada-4fca-9daa-b7ed4f5da935/dom/',
+      isTakenDown: false,
+      isConfirmed: false,
+      isEligibility: false,
+    },
+  ]);
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = event => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{minWidth: 650, width: 'fit-content'}} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">Implementers</TableCell>
+            <TableCell align="center">Date</TableCell>
+            <TableCell align="center">GitHub Report</TableCell>
+            <TableCell align="center">Domain</TableCell>
+            <TableCell align="center">UrlScan Link</TableCell>
+            <TableCell align="center">Taken Down</TableCell>
+            <TableCell align="center">Confirmed</TableCell>
+            <TableCell align="center">Eligibility</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {(rowsPerPage > 0
+            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            : rows
+          ).map((row, i) => (
+            <TableRow
+              key={`${row.implementers}-${i}`}
+              sx={{'&:last-child td, &:last-child th': {border: 0}}}
+            >
+              <TableCell component="th" scope="row" align="left">
+                {row.implementers}
+              </TableCell>
+              <TableCell align="left">{row.date.toLocaleDateString('en-US')}</TableCell>
+              <TableCell align="left">{row.ghReport}</TableCell>
+              <TableCell align="left">{row.domain}</TableCell>
+              <TableCell align="left">
+                {row.urlScanLink ? (
+                  <Link href={row.urlScanLink} target="_blank" rel="noreferrer noopener">
+                    {row.urlScanLink}
+                  </Link>
+                ) : (
+                  <Typography>No link!</Typography>
+                )}
+              </TableCell>
+              <TableCell align="center">{row.isTakenDown ? 'Yes' : 'No'}</TableCell>
+              <TableCell align="center">
+                {row.isConfirmed ? (
+                  <span role="img" aria-label="checked">
+                    ✅
+                  </span>
+                ) : (
+                  <span role="img" aria-label="unckecked">
+                    ❌
+                  </span>
+                )}
+              </TableCell>
+              <TableCell align="center">{row.isEligibility ? 'Yes' : 'No'}</TableCell>
+            </TableRow>
+          ))}
+
+          {emptyRows > 0 && (
+            <TableRow style={{height: 53 * emptyRows}}>
+              <TableCell colSpan={6} />
+            </TableRow>
+          )}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25, {label: 'All', value: -1}]}
+              colSpan={10}
+              count={rows.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              SelectProps={{
+                inputProps: {
+                  'aria-label': 'rows per page',
+                },
+                native: true,
+              }}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+              ActionsComponent={TablePaginationActions}
+            />
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default OverviewTable;
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,108 @@
+import AggregatesIcon from '@/icons/AggregatesIcon.svg';
+import CuratorsIcon from '@/icons/CuratorsIcon.svg';
+import DashboardIcon from '@/icons/DashboardIcon.svg';
+import GoalsIcon from '@/icons/GoalsIcon.svg';
+import ImplementersIcon from '@/icons/ImplementersIcon.svg';
+import LogoutIcon from '@/icons/LogoutIcon.svg';
+import RewardsIcon from '@/icons/RewardsIcon.svg';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import SvgIcon from '@mui/material/SvgIcon';
+
+import Image from 'next/image';
+
+const Sidebar = () => {
+  return (
+    <Box
+      component="div"
+      sx={{
+        width: '100%',
+        height: '100vh',
+        maxWidth: 221,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        background:
+          'linear-gradient(177.83deg, rgba(230, 0, 122, 0.99) -5.55%, rgba(230, 0, 122, 0.99) 53.24%, rgba(96, 0, 51, 0.85) 131.15%);',
+      }}
+    >
+      <div className="flex justify-center items-center mb-12 mt-6">
+        <Image src="/icons/PolkadotWhiteIcon.svg" alt="Polkadot-logo" width={83} height={83} />
+      </div>
+      <div>
+        <nav id="main-sidebar" aria-label="main-sidebar">
+          <List>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <SvgIcon component={DashboardIcon} inheritViewBox />
+                </ListItemIcon>
+                <ListItemText primary="Dashboard" primaryTypographyProps={{color: 'white'}} />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <SvgIcon component={ImplementersIcon} inheritViewBox />
+                </ListItemIcon>
+                <ListItemText primary="Implementers" primaryTypographyProps={{color: 'white'}} />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <SvgIcon component={CuratorsIcon} inheritViewBox />
+                </ListItemIcon>
+                <ListItemText primary="Curators" primaryTypographyProps={{color: 'white'}} />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <SvgIcon component={RewardsIcon} inheritViewBox />
+                </ListItemIcon>
+                <ListItemText primary="Rewards" primaryTypographyProps={{color: 'white'}} />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <SvgIcon component={AggregatesIcon} inheritViewBox />
+                </ListItemIcon>
+                <ListItemText primary="Aggregates" primaryTypographyProps={{color: 'white'}} />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <SvgIcon component={GoalsIcon} inheritViewBox />
+                </ListItemIcon>
+                <ListItemText primary="Goals" primaryTypographyProps={{color: 'white'}} />
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </nav>
+      </div>
+      <div className="mt-auto mb-12">
+        <nav id="logout-nav" aria-label="logout-nav">
+          <List>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <SvgIcon component={LogoutIcon} inheritViewBox />
+                </ListItemIcon>
+                <ListItemText primary="Logout" primaryTypographyProps={{color: 'white'}} />
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </nav>
+      </div>
+    </Box>
+  );
+};
+
+export default Sidebar;
