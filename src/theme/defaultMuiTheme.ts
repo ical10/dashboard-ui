711edf9d918e66e43a468aaf398d3baca1312034
ical10 <mochhusnir@gmail.com>
2022-08-28 12:04:20 +0700

    feat: overrode mui color palette

---
+++ src/theme/defaultMuiTheme.ts
@@ -0,0 +1,11 @@
+import {createTheme} from '@mui/material/styles';
+
+const muiTheme = createTheme({
+  palette: {
+    primary: {
+      main: 'hsla(328, 100%, 39%, 0.91)',
+    },
+  },
+});
+
+export default muiTheme;
