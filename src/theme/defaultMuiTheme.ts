88acfdd851e20237c7f08ed09b21755fe7807c04
ical10 <mochhusnir@gmail.com>
2022-08-28 12:41:13 +0700

    feat: restyled font

--- src/theme/defaultMuiTheme.ts
+++ src/theme/defaultMuiTheme.ts
@@ -1,9 +1,33 @@
 import {createTheme} from '@mui/material/styles';
 
+const font = "'Lato', sans-serif";
+
 const muiTheme = createTheme({
+  typography: {
+    fontFamily: font,
+    button: {
+      textTransform: 'none',
+    },
+  },
   palette: {
     primary: {
       main: 'hsla(328, 100%, 39%, 0.91)',
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none',
+
+          fontFamily: font,
+          fontWeight: 700,
+          fontSize: '28px',
+          lineHeight: '33.6px',
+
+          color: 'white',
+        },
+      },
     },
   },
 });
